07191635553f030d3d699f3bff714e48c9af84d8
Harihar <hari.harihar20@gmail.com>
2023-02-03 15:25:29 +0530

    template component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TemplateFormComponent } from './components/template-form/template-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateFormComponent
   ],
   imports: [
     BrowserModule,
